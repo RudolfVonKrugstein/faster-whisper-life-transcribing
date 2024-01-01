f9920a048575f6befba090c6236c1de9de7dbd18
Nathan Hüsken <nathan@huesken.org>
2024-01-01 19:46:45 +0100

    wip

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from transcribe import model
 import logging
 from decoder.pcm import PCMDecoder
 from decoder.opus import OpusDecoder
+from .transcribe.life.worker import TranscribeWorker
 
 from numpy.typing import NDArray
 
@@ -16,7 +17,8 @@ logging.basicConfig(level=logging.DEBUG)
 app = FastAPI()
 
 root = os.path.dirname(__file__)
-app.mount('/ui', StaticFiles(directory=os.path.join(root, 'ui')), name='ui')
+app.mount("/ui", StaticFiles(directory=os.path.join(root, "ui")), name="ui")
+
 
 async def get_body(request: Request) -> NDArray[nd.float32]:
     """
@@ -26,18 +28,30 @@ async def get_body(request: Request) -> NDArray[nd.float32]:
     if request.query_params.get("raw", "True").lower() == "true":
         return nd.frombuffer(await request.body(), dtype=nd.float32)
     else:
-        return decode_audio(io.BytesIO(await request.body()), sampling_rate=16000)
+        return decode_audio(
+            io.BytesIO(await request.body()), sampling_rate=16000, split_stereo=False
+        )
+
 
 @app.post("/transcribe")
-def transcribe(data: NDArray[nd.float32] = Depends(get_body), lang: str = "en", prompt: str|None=None):
+def transcribe(
+    data: NDArray[nd.float32] = Depends(get_body),
+    lang: str = "en",
+    prompt: str | None = None,
+):
     """
     Transcribe endpoint, taking the data in RAW float32 format
     and running transcribe on it.
     """
     return model.transcribe(data, lang, prompt)
 
+
 @app.post("/translate")
-def translate(data: NDArray[nd.float32] = Depends(get_body), lang: str = "en", prompt: str|None=None):
+def translate(
+    data: NDArray[nd.float32] = Depends(get_body),
+    lang: str = "en",
+    prompt: str | None = None,
+):
     """
     Transcribe endpoint, taking the data in RAW float32 format
     and running transcribe on it.
@@ -47,19 +61,17 @@ def translate(data: NDArray[nd.float32] = Depends(get_body), lang: str = "en", p
 
 @app.websocket("/transcribe_life")
 async def websocket_endpoint(
-        websocket: WebSocket,
-        mediaType = "raw",
-        lang: str = "en",
-        prompt: str|None=None
-        ):
-
+    websocket: WebSocket, mediaType="raw", lang: str = "en", prompt: str | None = None
+):
     decoder = None
     if mediaType == "raw":
         decoder = PCMDecoder()
     if mediaType == "opus":
         decoder = OpusDecoder()
-    if decoder == None:
-        raise HTTPException(status_code=401, detail=f"Unsupported mediaType {mediaType}")
+    if decoder is None:
+        raise HTTPException(
+            status_code=401, detail=f"Unsupported mediaType {mediaType}"
+        )
     await websocket.accept()
 
     worker = TranscribeWorker(decoder, lang=lang, prompt=prompt)
